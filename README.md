dc794b367e38f99dc1ad81a7411263eb3376beaf
Kevin Fu <kevin.fu808@gmail.com>
2022-01-05 00:40:42 -0800

    Created migration script and README

---
+++ README.md
@@ -0,0 +1,1 @@
+# A repo for scripts for migrating Github repos primary branch names from "master" to "main"
---
+++ migrate.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Migrating repo at local directory location [$1] to use 'main' instead of 'master' as the default branch name"
+
+# Important Variables
+PREVIOUS_DEFAULT_BRANCH_NAME=master
+NEW_DEFAULT_BRANCH_NAME=main
+
+# Save current directory
+CURRENT_DIR=$(pwd)
+echo "Executing script from current directory: $CURRENT_DIR"
+
+# Jump to the desired directory
+cd $1
+
+git branch -m master $NEW_DEFAULT_BRANCH_NAME
+git status
+git push -u origin $NEW_DEFAULT_BRANCH_NAME
+
+# Modifying the Github default branch name.
+# This is necessary in order to delete the previous default branch name
+gh repo edit --default-branch $NEW_DEFAULT_BRANCH_NAME
+
+git push origin --delete $PREVIOUS_DEFAULT_BRANCH_NAME
+
+# Jump back to the original current directory
+echo "Returning back to the original current directory: $CURRENT_DIRR"
+cd $CURRENT_DIR
